97e1dbc1beec78cd5f674fd0d707b1f59add80f3
Micha Merkli <micha.merkli18@gmail.com>
2017-09-23 01:08:15 +0200

    w3-CSS eingebunden, Login Design angepasst

--- eclipse_ws/class/model.class.php
+++ eclipse_ws/class/model.class.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 abstract class model
 {
@@ -8,13 +8,13 @@ abstract class model
 	protected $show_title = true;
 	protected $has_content_wraper = true;
 	protected $has_cards = false;
-	
+
 	protected $pdo_single;
 	protected $session_single;
 	protected $function_single;
 // 	protected $dict_single;
 	protected $constant_single;
-	
+
 	public function __construct($params)
 	{
 		$this->pdo_single 		= pdo_singleton::getInstance();
@@ -22,46 +22,46 @@ abstract class model
 		$this->function_single 	= function_singleton::getInstance();
 // 		$this->dict_single 		= dict_singleton::getInstance();
 		$this->constant_single 	= constant_singleton::getInstance();
-		
+
 		$this->init($params);
 	}
-	
+
 	abstract protected function init($params);
-	
+
 	abstract protected function createContent();
-	
+
 	public function createPage()
-	{	
+	{
 		// only execute init. no content displayed
 		if($this->session_single->isAjaxRequest())
 		{
 			return;
 		}
-		
+
 		$this->createHead();
-		
-		// Cover Container	
+
+		// Cover Container
 		?>
 		<div id="dropdown_container"></div>
-		
-		<div id="none" style="display: none;"></div> 
-		
-		<div id="cover_background"></div> 
+
+		<div id="none" style="display: none;"></div>
+
+		<div id="cover_background"></div>
 		<div id="cover">
 			<div id="cover_container">
-			</div> 
-		</div> 
-		
-		<?php 		
-		$navigation_page = new navigation_model(array()); 
-		$navigation_page->createContent(); 
-		
+			</div>
+		</div>
+
+		<?php
+		$navigation_page = new navigation_model(array());
+		$navigation_page->createContent();
+
 		?>
-		
+
 		<div class="app-body">
-			
+
 			<main class="main">
-			
+
 				<div class="container-fluid">
 					<div id="ui-view">
 						<div class="row">
@@ -73,7 +73,7 @@ abstract class model
 										<hr class="mb-4">
 
 										<?php $this->createContent(); ?>
-										
+
 									</div>
 								</div>
 							</div>
@@ -81,14 +81,14 @@ abstract class model
 					</div>
 				</div>
 				<!-- /.conainer-fluid -->
-			
+
 			</main>
-		
+
 		</div> <!-- END div.app-body -->
-		<?php 
-		$footer_page = new footer_model(array()); 
-		$footer_page->createContent(); 
-		
+		<?php
+		$footer_page = new footer_model(array());
+		$footer_page->createContent();
+
 		$this->createEnd();
 	}
 
@@ -97,7 +97,7 @@ abstract class model
 		<div class="container-fluid">
 			<div class="row cover_title_row">
 				<div class="col-sm-11">
-					<?php /* 
+					<?php /*
 					<span id='cover_title'><?php echo $this->title; ?></span>
 					*/ ?>
 				</div>
@@ -114,84 +114,87 @@ abstract class model
 			</div>
 		</div>
 		<script type="text/javascript">bind_events();</script>
-		<?php 
-	}	
-	
+		<?php
+	}
+
 	private function createHead()
 	{	?>
 <!DOCTYPE html>
 <html lang="de">
 	<head>
-	
+
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	
+
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<link rel="icon" href="">
-		
-		<title>Swape Space</title>
-		
-		<link rel="stylesheet" href="css/bootstrap.css">
-		<link rel="stylesheet" href="css/jquery-ui.css">		
 
-		
+		<title>Swape Space</title>
+
+		<link rel="stylesheet" href="css/w3.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/jquery-ui.css">
+
+
 		<!-- My CSS Sheets -->
 		<link rel="stylesheet" href="css/myCSS/style.css">
 		<link rel="stylesheet" href="css/myCSS/cover.css">
 
-		
+
 	</head>
 	<body class="app header-fixed aside-menu-fixed aside-menu-hidden sidebar-hidden">
-	<?php 
+	<?php
 	}
-	
+
 	private function createEnd()
 	{	?>
 		<!-- Bootstrap and necessary plugins -->
 		<script type="text/javascript" src="js/jquery-2.2.3.js"></script>
 		<script type="text/javascript" src="js/jquery-ui.js"></script>
 		<script type="text/javascript" src="js/bootstrap.js"></script>
-		
+
 		<!-- Firmenradar specific Scripts -->
-		
+
 		<script type="text/javascript" src="js/file_upload.js"></script>
 		<script type="text/javascript" src="js/main.js"></script>
 		<script type="text/javascript" src="js/jquery_binder.js"></script>
-		
+
 	</body>
 </html>
-		<?php 
+		<?php
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param string $target - pagename
 	 */
 	protected function setBackButtonTarget($target)
 	{	$this->back['target'] = $target; }
 
 // 	protected function include404()
-// 	{	$mypage = new not_found_page(array()); 
-// 		$mypage->createCover(); 
+// 	{	$mypage = new not_found_page(array());
+// 		$mypage->createCover();
 // 	}
-	
+
 	protected function setTitle($title)
 	{	$this->title = $title; }
-	
+
 	protected function setTitleClass($title_class)
 	{	$this->title_class = $title_class; }
-	
+
 	protected function hideTitleRow()
 	{	$this->show_title = false; }
-	
+
 	protected function hideContentWraper()
 	{	$this->has_content_wraper = false; }
-	
+
 	protected function hasContentWraper()
 	{	return $this->has_content_wraper; }
-	
 
-	
+
+
 }
 ?>
--- eclipse_ws/model/login.model.php
+++ eclipse_ws/model/login.model.php
@@ -2,33 +2,33 @@
 
 class login_model extends model
 {
-	
+
 protected function init($params)
-	{		
+	{
 // 		$this->setTitle($this->dict_single->dict('login'));
 
 		if($_POST && isset($_POST['dc_nr'])	&& isset($_POST['password']))
 		{
 			// pw reset?
-			
+
 			if(!$_POST['dc_nr'] || $_POST['password']){
-				
+
 				$user_obj = new damage_case(0);
 				$status = $user_obj->login($_POST['dc_nr'], $_POST['password']);
-					
+
 				if($status == 1){
 					die('<script type="text/javascript">window.location = "dashboard";</script>');
 				}
 			}
-			
+
 			die('Ihre Eingaben waren fehlerhaft. Bitte versuchen Sie es erneut.');
 		}
 	}
-	
+
 	protected function createContent()
-	{	
+	{
 		?>
-		<div class="card-group mb-0">
+		<!--<div class="card-group mb-0">
 			<div class="card p-4">
 				<div class="card-block">
 					<form id='login_form' action='dashboard' method='post' onsubmit='return checkLogin();'>
@@ -59,11 +59,28 @@ protected function init($params)
 					</form>
 				</div>
 			</div>
-		</div>
-		<?php 
-		
+		</div>-->
+    <div class="w3-card-4" style="max-width:600px">
+      <form class="w3-container" id='login_form' action='dashboard' method='post' onsubmit='return checkLogin();'>
+        <div class="w3-section">
+          <label><b>Schadensfall-Nr</b></label>
+          <input class="w3-input w3-border w3-margin-bottom form-control" id="login_email" type="text" placeholder="Nr." name="dc_nr" required>
+          <label><b>Passwort</b></label>
+          <input class="w3-input w3-border form-control" type="password" placeholder="Passwort" name="password" required>
+          <button name="submit" value="login" class="w3-button w3-block w3-green w3-section w3-padding" type="submit">Login</button>
+          <input class="w3-check w3-margin-top" type="checkbox" checked="checked"> Erinnere mich
+        </div>
+      </form>
+
+      <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+        <button type="button" class="w3-button w3-red">Cancel</button>
+      </div>
+
+    </div>
+		<?php
+
 	}
-	
-	
+
+
 }
 ?>
