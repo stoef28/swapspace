0cefc1d8253111ec13ac226b14396e4f5f9e119a
boescste <boescste@students.zhaw.ch>
2017-09-23 13:48:35 +0200

    Changes

--- eclipse_ws/model/dashboard.model.php
+++ eclipse_ws/model/dashboard.model.php
@@ -2,25 +2,26 @@
 
 class dashboard_model extends model
 {
-	
+
 	protected function init($params){
-		
+
 		$this->showChat();
-		
+
 		unset($_SESSION['employee_id']);
 
 		$_SESSION['last_message_id'] = 0;
 	}
 	protected function createContent(){
-	
+
 		$dc_ide = $this->function_single->encrypt($this->session_single->getActiveUser()->getId(), 'damage_case_id');
 		?>
-		
-			<div class="w3-half w3-left-align">
+
+			<div class="w3-half w3-left-align backgroundpic">
                 <div class="w3-card-4 w3-margin" style="height:300px">
                     <div class="w3-container">
-                        <h1>Herzlich Willkommen auf Swap Space!</h1>
-                        <p>Alle ihre Daten an einem Ort. Wir wünschen ihnen gute Besserung.</p>
+                        <h1>Willkommen auf Swap Space!</h1>
+                        <p>Hier finden Sie alle Informationen auf einen Blick. <br>
+							Wir wünschen Ihnen gute Besserung.</p>
                     </div>
                 </div>
             </div>
@@ -37,11 +38,23 @@ class dashboard_model extends model
                             </tr>
                             <tr>
                                 <th style="padding-left: 5px">Ansprechperson:</th>
-                                <th>Cyrill Hasler</th>
+                                <th>Michele Eichenberger</th>
+                            </tr>
+							<tr>
+                                <th style="padding-left: 5px">Tel:</th>
+                                <th>58 285 76 97</th>
+                            </tr>
+							<tr>
+                                <th style="padding-left: 5px">E-Mail:</th>
+                                <th>michele.eichenberger@baloise.ch</th>
                             </tr>
                             <tr>
                                 <th style="padding-left: 5px">Status:</th>
                                 <th>Abklärung Sachverhalt</th>
+                            </tr>
+							<tr>
+                                <th style="padding-left: 5px">Leistungsübersicht:</th>
+                                <th>Sehe "Meine Dokumente"</th>
                             </tr>
                         </table>
 
@@ -53,9 +66,9 @@ class dashboard_model extends model
                 <div class="w3-card-4 w3-margin" style="height:300px">
                     <div class="w3-container">
                         <h1>Pendenzen</h1>
-                        <p>Keine Pendenzen ausstehend</p>
+                        <p>- Ärztliches Zeugnis einreichen</p>
 
-                        <a href="survey&survey_ide=7i9dgLBgDP6KUYwj0pgFPQosqtjM04CgFcKpITfTQPg-">Zur Umfrage</a>
+                        <!--<a href="survey&survey_ide=7i9dgLBgDP6KUYwj0pgFPQosqtjM04CgFcKpITfTQPg-">Zur Umfrage</a>-->
 
                     </div>
                 </div>
@@ -66,7 +79,10 @@ class dashboard_model extends model
                     <div class="w3-container">
                         <h1>Status</h1>
                         <h3>Abklärung Sachverhalt</h3>
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                        <p>Wir sind daran Ihren Fall zu bearbeiten. Es ist uns ein Anliegen, Sie hilfreich zu unterstützen und die Bearbeitung Ihres Falls so unkompliziert wie möglich zu gestalten. Zu diesem Zweck möchten wir uns zunächst ein genaues Bild über die vorliegende Situation machen, wozu wir noch einige Angaben von Ihnen benötigen.
+
+							<br>Vielen Dank für Ihre Unterstützung.<br>
+
                         <a href="status_overview"> weitere infos</a></p>
                     </div>
                 </div>
@@ -87,11 +103,9 @@ class dashboard_model extends model
 
                         <table class="w3-striped w3-table w3-bordered">
                             <tr>
-                                <td><i class="fa fa-file-pdf-o fa-lg w3-margin-right" ></i>Ärztliches Zeugnis.pdf</td>
+                                <td><i class="fa fa-file-pdf-o fa-lg w3-margin-right" ></i>Leistungsübersicht.pdf</td>
                             </tr>
-                            <tr>
-                                <td><i class="fa fa-file-pdf-o fa-lg w3-margin-right" ></i>Document 1.pdf</td>
-                            </tr>
+
                             <tr>
                                 <td><i class="fa fa-file-image-o fa-lg w3-margin-right" ></i>Unfallbild_1.jpg</td>
                             </tr>
@@ -103,11 +117,29 @@ class dashboard_model extends model
                     </div>
                 </div>
             </div>
-		
-		<?php 
-		
+
+
+
+
+
+			<div class="w3-half w3-left-align">
+				<div class="w3-card-4 w3-margin" style="height:300px">
+					<div class="w3-container">
+
+						<img class="w3-image w3-center w3-content" src="pics/Banner.png" alt="" style="height:300px">
+
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+		<?php
+
 	}
-	
-	
+
+
 }
 ?>
